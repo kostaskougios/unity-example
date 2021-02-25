1e44197a4f15cb52e0cc7c99c1e650aa755ab131
Kostas Kougios <kostas.kougios@googlemail.com>
2021-02-25 22:18:32 +0000

    rotation

--- Assets/Scripts/CarScript.cs
+++ Assets/Scripts/CarScript.cs
@@ -6,10 +6,15 @@ public class CarScript : MonoBehaviour
 {
     [SerializeField]
     float speed = 1;
+
+    [SerializeField]
+    float rotateSpeed = 10;
+
     // Update is called once per frame
     void Update()
     {
-        float dt=Time.deltaTime;
-        transform.Translate(1* dt, speed * dt, 0);
+        float dt = Time.deltaTime;
+        transform.Translate(1 * dt, speed * dt, 0);
+        transform.Rotate(0, 0, rotateSpeed * dt);
     }
 }
