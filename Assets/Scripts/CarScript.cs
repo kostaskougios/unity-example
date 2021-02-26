cc4f9a9cf31829de7f4bffe282842987c7591d41
Kostas Kougios <kostas.kougios@googlemail.com>
2021-02-26 21:28:56 +0000

    speed adjustments

--- Assets/Scripts/CarScript.cs
+++ Assets/Scripts/CarScript.cs
@@ -5,26 +5,32 @@ using UnityEngine;
 public class CarScript : MonoBehaviour
 {
     [SerializeField]
-    float speed = 1;
+    float maxSpeedMilesPerHour = 100;
 
     [SerializeField]
-    float rotateSpeed = 10;
+    float rotateSpeed = 20;
 
+    float currentSpeed = 0;
     // Update is called once per frame
     void Update()
     {
         float dt = Time.deltaTime;
-        float forward = 0;
+        float maxSpeed = maxSpeedMilesPerHour / 2000;
 
-        if (Input.GetKey(KeyCode.W)) forward = speed * dt;
-        if (Input.GetKey(KeyCode.S)) forward = -speed * dt;
+        if (Input.GetKey(KeyCode.W)) currentSpeed += dt;
+        else if (Input.GetKey(KeyCode.S)) currentSpeed -= dt;
+        else if (currentSpeed > 0) currentSpeed -= dt; else if (currentSpeed < 0) currentSpeed += dt;
+
+        if (currentSpeed > maxSpeed) currentSpeed = maxSpeed;
+        if (currentSpeed < -maxSpeed) currentSpeed = 0;
+        if (Mathf.Abs(currentSpeed) < dt) currentSpeed = 0;
 
         float turn = 0;
 
         if (Input.GetKey(KeyCode.A)) turn = -rotateSpeed * dt;
         if (Input.GetKey(KeyCode.D)) turn = +rotateSpeed * dt;
 
-        transform.Translate(0, forward, 0);
+        transform.Translate(0, currentSpeed, 0);
         transform.Rotate(0, 0, turn);
     }
 }
