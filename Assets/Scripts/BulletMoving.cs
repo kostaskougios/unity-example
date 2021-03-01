57a9043b5f6f083351d4ef3b4f312b587da3942c
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2021-03-01 21:11:16 +0000

    optimize BulletMoving.cs

--- Assets/Scripts/BulletMoving.cs
+++ Assets/Scripts/BulletMoving.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,16 @@ using UnityEngine;
 public class BulletMoving : MonoBehaviour
 {
     [SerializeField] float speed = 10;
+    [SerializeField] float destroyAfter = 2;
 
-    float activeTime = 0;
     void Update()
     {
         var dt = Time.deltaTime;
         transform.Translate(speed * dt, 0, 0);
-        activeTime += dt;
-        if (activeTime > 3) Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        Destroy(gameObject, destroyAfter);
     }
 }
