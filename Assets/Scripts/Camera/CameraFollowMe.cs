dde784b7e8afe758f2bbe0cc0ca05d3d65c81040
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2021-03-02 21:23:24 +0000

    refactoring

--- Assets/Scripts/Camera/CameraFollowMe.cs
+++ Assets/Scripts/Camera/CameraFollowMe.cs
@@ -1,17 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-/**
+namespace Camera
+{
+    /**
  * Add this on a camera to follow the followMe game object
  */
-public class CameraFollowMe : MonoBehaviour
-{
-    [SerializeField] private GameObject followMe;
-
-    void Update()
+    public class CameraFollowMe : MonoBehaviour
     {
-        var position = followMe.transform.position;
-        transform.position = new Vector3(position.x + 20, position.y + 10, position.z+15);
+        [SerializeField] private GameObject followMe;
+
+        private float i = 0;
+        void Update()
+        {
+            var position = followMe.transform.position;
+            i += 0.2f;
+            transform.position = new Vector3(position.x + 20, position.y + 10, position.z + 15 );
+        }
     }
 }
