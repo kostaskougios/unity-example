875269712f626f711c439be1a29ba800b314bb7b
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2021-03-18 00:00:07 +0000

    -

---
+++ Assets/Scripts/Projectiles/BulletHittingObject.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+namespace Projectiles
+{
+    public class BulletHittingObject : MonoBehaviour
+    {
+        public GameObject explosion;
+
+        private void OnCollisionEnter(Collision other)
+        {
+            var p = Instantiate(explosion);
+            p.transform.position = transform.position;
+            p.transform.rotation = transform.rotation;
+            Destroy(p, 2);
+        }
+    }
+}
