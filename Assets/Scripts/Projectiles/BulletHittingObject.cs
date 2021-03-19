756085559bb982301203c31e6a031ac2296053a7
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2021-03-19 22:34:06 +0000

    -

--- Assets/Scripts/Projectiles/BulletHittingObject.cs
+++ Assets/Scripts/Projectiles/BulletHittingObject.cs
@@ -7,8 +7,16 @@ namespace Projectiles
     {
         public GameObject explosion;
 
+        private AudioSource audioSource;
+
+        private void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
         private void OnCollisionEnter(Collision other)
         {
+            AudioSource.PlayClipAtPoint(audioSource.clip,transform.position);
             var p = Instantiate(explosion);
             var t = transform;
             p.transform.position = t.position;
